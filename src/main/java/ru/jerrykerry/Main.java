7981f3da82cc82c11ad1aebf71ba0937d927516d
Артем <jerrykerry@mail.ru>
2025-06-13 22:43:57 +0300

    Update Main.java

--- src/main/java/ru/jerrykerry/Main.java
+++ src/main/java/ru/jerrykerry/Main.java
@@ -4,8 +4,10 @@ package ru.jerrykerry;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        private static String msg = "Hello"
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa guru !!!!!");
+        System.out.printf(msg);
     }
 }
