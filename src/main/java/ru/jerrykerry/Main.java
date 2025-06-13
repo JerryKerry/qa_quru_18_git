c8e22a749282a6da6b981a0d9bb18279c1b6dfd4
Артем <jerrykerry@mail.ru>
2025-06-13 23:12:58 +0300

    Update Main.java

--- src/main/java/ru/jerrykerry/Main.java
+++ src/main/java/ru/jerrykerry/Main.java
@@ -3,7 +3,7 @@ package ru.jerrykerry;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static String msg = "Hello";
+    private static String msg = "";
     public static void main(String[] args) {
 
         
